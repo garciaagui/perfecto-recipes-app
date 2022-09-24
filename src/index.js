2c4bcf04d39abb3e56189c2df4020c0d615a75f9
Ruham Leal <ruhamxlpro@hotmail.com>
2022-09-24 12:41:59 -0300

    Adiciona validaçao nas rotas(Route) pois nao estavam levando pra rota corretamente

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={ store }>
-      <App />
-    </Provider>
-  </BrowserRouter>,
+  <Provider store={ store }>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
