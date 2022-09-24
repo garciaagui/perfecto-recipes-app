a71e2ecc9989be863432983da18f23d87a5315ac
Guilherme Garcia <garciaguig@gmail.com>
2022-09-24 07:05:18 -0300

    Conclusão do Requisito 01

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,6 +4,37 @@ function Login() {
   return (
     <form>
       <h2>Login</h2>
+      <label htmlFor="email-input">
+        <input
+          type="email"
+          id="email-input"
+          placeholder="E-mail"
+          data-testid="email-input"
+          // value={  }
+          // onChange={  }
+        />
+      </label>
+
+      <label htmlFor="password-input">
+        <input
+          type="password"
+          id="password-input"
+          placeholder="Senha"
+          data-testid="password-input"
+          // value={  }
+          // onChange={  }
+        />
+      </label>
+
+      <button
+        type="submit"
+        data-testid="login-submit-btn"
+        // disabled={  }
+        // onClick={  }
+      >
+        Enter
+      </button>
+
     </form>
   );
 }
