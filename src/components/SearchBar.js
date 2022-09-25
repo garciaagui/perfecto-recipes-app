625d3259b75b37b9ec20887a9b652ffe565cc62e
Ruham Leal <ruhamxlpro@hotmail.com>
2022-09-25 14:00:01 -0300

    Adiciona requisito 10

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,9 +2,44 @@ import React from 'react';
 
 function SearchBar() {
   return (
-    <div>
+    <section>
       <input data-testid="search-input" type="text" placeholder="Search" />
-    </div>
+      <div>
+        <label htmlFor="ingredient">
+          <input
+            data-testid="ingredient-search-radio"
+            name="search_filter"
+            type="radio"
+            id="ingredient"
+          />
+          Ingredient
+        </label>
+        <label htmlFor="name-search">
+          <input
+            data-testid="name-search-radio"
+            name="search_filter"
+            type="radio"
+            id="name-search"
+          />
+          Name
+        </label>
+        <label htmlFor="first-letter-search">
+          <input
+            data-testid="first-letter-search-radio"
+            name="search_filter"
+            type="radio"
+            id="first-letter-search"
+          />
+          First Letter
+        </label>
+      </div>
+      <button
+        data-testid="exec-search-btn"
+        type="button"
+      >
+        Search
+      </button>
+    </section>
   );
 }
 
