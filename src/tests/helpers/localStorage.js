c2e250f2e27f4f11c3a710e639ee5b3bd8b1fcb8
TULIO VILACA DINIZ MARCO <sd36797@gescoeurope.fiatgesco.com>
2022-09-27 16:37:45 -0300

    requisitos perfil

---
+++ src/tests/helpers/localStorage.js
@@ -0,0 +1,15 @@
+export default function getEmailLocalStorage() {
+  const data = localStorage.getItem('user');
+  if (data === null) {
+    return { email: 'faça login' };
+  }
+  const retorno = JSON.parse(data);
+  return retorno;
+}
+
+export function clearLocalStorage() {
+  localStorage.removeItem('user');
+  localStorage.removeItem('token');
+  localStorage.removeItem('drinksToken');
+  localStorage.removeItem('mealsToken');
+}
