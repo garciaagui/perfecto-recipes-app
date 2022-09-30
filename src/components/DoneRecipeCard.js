7f5e0ecd81bff1af9f152fdd64c7d39c8e488722
Ruham Leal <ruhamxlpro@hotmail.com>
2022-09-30 13:26:03 -0300

    Adiciona requisitos 44-46

---
+++ src/components/DoneRecipeCard.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import shareButton from '../images/shareIcon.svg';
+
+function DoneRecipeCard({ recipe, index }) {
+  console.log(recipe);
+  if (recipe.type === 'meal') {
+    return (
+      <div>
+        <img
+          data-testid={ `${index}-horizontal-image` }
+          src={ recipe.image }
+          alt={ recipe.name }
+        />
+        <h2 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h2>
+        <h3
+          data-testid={ `${index}-horizontal-top-text` }
+        >
+          {`${recipe.nationality} - ${recipe.category}`}
+        </h3>
+        <h4 data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</h4>
+        { recipe.tags.map((tag) => (
+          <p data-testid={ `${index}-${tag}-horizontal-tag` } key={ tag }>{tag}</p>
+        )) }
+        <button
+          type="button"
+        >
+          <img
+            data-testid={ `${index}-horizontal-share-btn` }
+            src={ shareButton }
+            alt="Share Button"
+          />
+        </button>
+      </div>
+    );
+  } return (
+    <div>
+      <img
+        data-testid={ `${index}-horizontal-image` }
+        src={ recipe.image }
+        alt={ recipe.name }
+      />
+      <h2 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h2>
+      <h3 data-testid={ `${index}-horizontal-top-text` }>{recipe.alcoholicOrNot}</h3>
+      <h4 data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</h4>
+      { recipe.tags.map((tag) => (
+        <p data-testid={ `${index}-${tag}-horizontal-tag` } key={ tag }>{tag}</p>
+      )) }
+      <button
+        type="button"
+      >
+        <img
+          data-testid={ `${index}-horizontal-share-btn` }
+          src={ shareButton }
+          alt="Share Button"
+        />
+      </button>
+    </div>
+  );
+}
+
+DoneRecipeCard.propTypes = {
+  recipe: PropTypes.shape().isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+export default DoneRecipeCard;
--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import DoneRecipeCard from '../components/DoneRecipeCard';
 
 function DoneRecipes({ history }) {
+  const [doneRecipes, setDoneRecipes] = useState([]);
+
+  useEffect(() => {
+    if (doneRecipes !== null && doneRecipes.length === 0) {
+      setDoneRecipes(JSON.parse(localStorage.getItem('doneRecipes')));
+      /* setDoneRecipes([{
+        id: 'id-da-receita',
+        type: 'meal-ou-drink',
+        nationality: 'nacionalidade-da-receita-ou-texto-vazio',
+        category: 'categoria-da-receita-ou-texto-vazio',
+        alcoholicOrNot: 'alcoholic-ou-non-alcoholic-ou-texto-vazio',
+        name: 'nome-da-receita',
+        image: 'imagem-da-receita',
+        doneDate: 'quando-a-receita-foi-concluida',
+        tags: ['tag1', 'tag2'],
+      }]); */
+    }
+  });
+
+  const doneRecipesValid = doneRecipes !== null && doneRecipes.length > 0;
+
   return (
-    <div>
+    <main>
       <Header history={ history } />
-    </div>
+      <section>
+        <button data-testid="filter-by-all-btn" type="button">All</button>
+        <button data-testid="filter-by-meal-btn" type="button">Meals</button>
+        <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
+      </section>
+      <section>
+        { doneRecipesValid ? (
+          doneRecipes.map((recipe, index) => (
+            <DoneRecipeCard key={ recipe.id } recipe={ recipe } index={ index } />
+          ))
+        ) : (
+          <h1>Nao tem receitas prontas</h1>
+        )}
+      </section>
+    </main>
   );
 }
 
